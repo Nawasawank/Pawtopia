090ef4f81140a7d4d2204d973eaf369e5c7b5eee
Nawasawan Yenrompho <nawasawan.y@gmail.com>
2024-10-31 11:20:12 +0700

    add get user info for profile page

--- Backend/controllers/Info.controller.js
+++ Backend/controllers/Info.controller.js
@@ -5,7 +5,7 @@ const InfoController = {
         const { id: userId } = req.user;
 
         try {
-            const user = await InfoService.getUserInfo(userId);
+            const user = await InfoService.getUserProfile(userId);
 
             if (user.error) {
                 return res.status(500).json({ error: user.error });
@@ -14,6 +14,29 @@ const InfoController = {
             return res.status(200).json({ firstName: user.firstName, image: user.image });
         } catch (error) {
             console.error(`Error in getUserProfile: ${error.message}`);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+    async getUserInfo(req, res) {
+        const { id: userId } = req.user;
+    
+        try {
+            const user = await InfoService.getUserInfo(userId);
+    
+            if (user.error) {
+                return res.status(500).json({ error: user.error });
+            }
+    
+            return res.status(200).json({
+                user_id: userId,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                tel: user.tel,
+                image: user.image
+            });
+        } catch (error) {
+            console.error(`Error in getUserInfo: ${error.message}`);
             return res.status(500).json({ error: 'Internal Server Error' });
         }
     }
